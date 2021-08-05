55df753f493fa82f5aa15b42991232577b96be56
Lordva <lordva.pro@gmail.com>
2021-08-05 19:26:58 +0200

    written first version of install script

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+PKG="mctl"
+GIT_URL="https://github.com/lordva/mctl"
+INSTALL_DIR="/usr/local/mctl"
+
+install(){
+  if [ -e "${INSTALL_DIR}" ]; then
+    echo "${PKG} is already installed"
+    echo "Updating ${PKG} from git"
+    command git --git-dir="${INSTALL_DIR}/.git" fetch
+    command git --get-dir="${INSTALL_DIR}/.git" reset
+  else
+    echo "Dowloading ${PKG} from git to ${INSTALL_DIR}"
+    command git clone ${GIT_URL} ${INSTALL_DIR} || echo >&2 "Failed to clone"; exit 1
+    chmod -R 755 ${INSTALL_DIR}/servers 
+    chmod 755 ${INSTALL_DIR}/mctl
+    sudo ln -sf ${INSTALL_DIR}/mctl /usr/local/bin/mctl
+  fi
+}
+
+if git --version; then
+  install;
+else
+  echo >&2 "Failed to install, git is needed for installation"
+fi
+
+if [ -f "${INSTALL_DIR}/mctl" ]; then
+  echo
+  echo "Done!"
+  exit 0
+else
+  echo >&2
+  echo >&2 "Error on installation, ${INSTALL_DIR}/mctl not found"
+  echo >&2
+  exit 1
+fi
