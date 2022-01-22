7129ff913ce0a1b58afd143c1579849d7ee90617
Lordva <lordva.pro@gmail.com>
2022-01-22 11:06:06 +0100

    Improved Install script by adding more verbose logging Fixed bug where mclt would not install

--- install.sh
+++ install.sh
@@ -4,15 +4,21 @@ PKG="mctl"
 GIT_URL="https://github.com/lordva/mctl"
 INSTALL_DIR="/usr/local/mctl"
 
+RESET="\033[0m"         # Normal color
+RED="\033[0;31m"        # Errors
+GREEN="\033[0;32m"      # Sucess
+BOLD="\033[1;01m"
+
 install(){
+  echo -e "[ - ] INSTALLING MCTL FROM GIT:"
   if [ -e "${INSTALL_DIR}" ]; then
     echo "${PKG} is already installed"
     echo "Updating ${PKG} from git"
     command git --git-dir="${INSTALL_DIR}/.git" fetch || echo >&2 "Failed to fetch" ; exit 1
     command git --git-dir="${INSTALL_DIR}/.git" reset || echo >&2 "failed to reset" ; exit 1
   else
-    echo "Dowloading ${PKG} from git to ${INSTALL_DIR}"
-    command git clone ${GIT_URL} ${INSTALL_DIR} || echo >&2 "Failed to clone"; exit 1
+    echo "[ - ]Dowloading ${PKG} from git to ${INSTALL_DIR}"
+    command git clone ${GIT_URL} ${INSTALL_DIR} || echo >&2 "[${RED}✕${RESET}]Failed to clone"; exit 1
     chmod -R 755 ${INSTALL_DIR}/servers
     chmod 755 ${INSTALL_DIR}/mctl
     sudo ln -sf ${INSTALL_DIR}/mctl /usr/local/bin/mctl
@@ -20,24 +26,28 @@ install(){
 }
 
 check_requirements(){
-  if ! bash --help 2>&1 > /dev/null; then
-    echo >&2 "Error Bash is needed for mctl to work"
-  fi
+  echo -e "- Cheking for Git:"
   if ! git --version 2>&1 > /dev/null; then
     echo >&2 "Failed to install, git is needed for installation"
     exit 1
   fi
+  echo -e "OK"
+  echo -e "- Checking for TMUX:"
   if ! tmux -V 2>&1 > /dev/null; then
-    echo >&2 "Error tmux is needed for mctl to work"
+    echo >&2 -e "[${RED}✕${RESET}]Error tmux is needed for mctl to work"
     exit 1
   fi
+  echo -e "OK"
+  echo -e"[${GREEN}✔${RESET}] Requirements are installed"
 }
 
+echo -e "[ - ] Cheking for requirements..."
 check_requirements
+install
 
 if [ -f "${INSTALL_DIR}/mctl" ]; then
   echo
-  echo "Done!"
+  echo -e "[${GREEN}✔]Done!${RESET}"
   exit 0
 else
   echo >&2
